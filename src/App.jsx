0cec6888754f6ef5c18ac8543193a8c65fc5df8a
Jesusbm591 <20200746@uthh.edu.mx>
2025-04-07 17:10:01 -0600

    subo proyecto

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+import Header from './Modulos/header' 
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div  className="wall-wrapper">
+
+    <>
+    <Header className='Header'/>
+    
+      <>
+      <div>
+        <div>
+          <a target="_blank">
+            <img src="https://res.cloudinary.com/dje0ep7ig/image/upload/v1744059590/photo_5161191930099511036_y_dk6bbb.jpg" className="logo" alt="React logo" />
+          </a>
+        </div>
+          <h1 className='h1'>
+          Muebleria DMM
+          </h1>
+        <div className="card">
+          <button>
+          Trabajos Realizados
+          </button>
+          <p>
+          Para ver mas información visita los modulos del sistema
+          </p>
+        </div>
+        <p className="read-the-docs">
+          Bienvenidos a Muebleria De Madera Mobiliario
+        </p>
+        </div>
+      </>
+    
+    </>
+    </div>
+  )
+}
+export default App
---
+++ src/Modulos/header.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import "./Header.css";
+
+const Header = () => {
+  const [menuAbierto, setMenuAbierto] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuAbierto(!menuAbierto);
+  };
+
+  return (
+    <header className="header">
+      <button className="menu-toggle" onClick={toggleMenu}>
+        ☰
+      </button>
+
+     
+      <nav className={`nav ${menuAbierto ? "open" : ""}`}>
+        <ul>
+          <li><a href="#" onClick={() => setMenuAbierto(false)}>Inicio</a></li>
+          <li><a href="#" onClick={() => setMenuAbierto(false)}>Proyectos</a></li>
+          <li><a href="#" onClick={() => setMenuAbierto(false)}>Contacto</a></li>
+          <li><a href="#" onClick={() => setMenuAbierto(false)}></a></li>
+          <li><a href="#" onClick={() => setMenuAbierto(false)}>Contacto</a></li>
+        </ul>
+      </nav>
+
+      {menuAbierto && <div className="overlay" onClick={toggleMenu}></div>}
+    </header>
+  );
+};
+
+export default Header;
