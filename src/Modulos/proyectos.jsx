f24dc8ab6ba576ad4d4ab0f3cc9d17328152dee7
Jesusbm591 <20200746@uthh.edu.mx>
2025-04-08 16:37:09 -0600

    Vista Proyectos

--- src/Modulos/proyectos.jsx
+++ src/Modulos/proyectos.jsx
@@ -1,16 +1,42 @@
 import React from "react";
-import '../App.css'
+import "../App.css";
 import Header from "./header";
-const Proyectos = () => {
-    return(
-        <>
-        <div className="wall-wrapper">
-            <Header />
-        <h1>Proyectos realizados :)</h1>
-        </div>
-        </>
-    );
 
-}
+const proyectos = [
+  {
+    id: 1,
+    title: "Mueble de cocina",
+    image: "https://res.cloudinary.com/dje0ep7ig/image/upload/v1744150907/photo_5163914101846552225_y_hvmint.jpg",
+  },
+  {
+    id: 2,
+    title: "Closet de madera",
+    image: "https://res.cloudinary.com/dje0ep7ig/image/upload/v1744150907/photo_5163914101846552226_y_mns6gu.jpg",
+  },
+  {
+    id: 3,
+    title: "Entretenimiento",
+    image: "https://res.cloudinary.com/dje0ep7ig/image/upload/v1744150907/photo_5163914101846552227_y_uaw1ug.jpg",
+  },
+];
+
+const Proyectos = () => {
+  return (
+    <div className="wall-wrapper">
+      <Header />
+      <div className="Card">
+        <h1>Proyectos realizados 🙂</h1>
+        <div className="card-grid">
+          {proyectos.map((p) => (
+            <div className="image-card" key={p.id}>
+              <img src={p.image} alt={p.title} />
+              <h3>{p.title}</h3>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Proyectos;
